fea997b0e9bf4c24f068832f7ea5782f7af2145f
Anurag De <anuragde2004@gmail.com>
2025-09-18 12:58:52 +0530

    Created new API to get role, loginDate and loginTime fromt jwtToken

--- backend/routes/alluserRoutes.js
+++ backend/routes/alluserRoutes.js
@@ -2,11 +2,14 @@ const express = require('express');
 const { protect } = require('../middleware/authMiddleware');
 const { authorize } = require('../middleware/roleMiddleware');
 const { getUserDataByEmail } = require('../controllers/alluserController');
+const { getRoleAndTimefromToken } = require('../controllers/authController');
 const router = express.Router();
 
 router.use(protect)
 router.use(authorize("student","teacher","admin"))
 
 router.get('/user-data',getUserDataByEmail)
+router.get('/get-role', getRoleAndTimefromToken);
+
 
 module.exports = router;
