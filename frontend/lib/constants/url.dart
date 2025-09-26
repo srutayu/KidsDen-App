63f0be3b58d0e557dd096e26c4c0e36e9762d1ac
Anurag De <anuragde2004@gmail.com>
2025-09-26 11:11:22 +0530

    updated baseURL

--- frontend/lib/constants/url.dart
+++ frontend/lib/constants/url.dart
@@ -1,6 +1,6 @@
 class URL {
   static final String baseHost = '192.168.0.131';
-  static final String baseURL = 'http://$baseHost:5000/api';
+  static final String baseURL = 'http://$baseHost:3000/api';
   static final String chatURL = 'http://$baseHost:8000/api';
   static final String socketURL = 'http://$baseHost:8000';
 }
